7580300162ade7e1992e683a9b2edc1222d884ad
ccysfn <jessecao911@gmail.com>
2019-08-04 17:33:25 +0800

    f4

--- app.js
+++ app.js
@@ -8,29 +8,32 @@ const port = 9002;
 const add='https://api.weixin.qq.com/sns/jscode2session';
 var v_data='gggg';
 var jcode='jjjj';
+
+
 // 实现唯一的一个中间件，对于所有请求，都输出 "Response from express"
-app.get('/',(req,res)=>{
-  //res.send(req.param('code'));
-	jcode=req.param('code');
-   // res.send(jcode);
-	 axios.get('https://api.weixin.qq.com/sns/jscode2session',{
-		params:{
-			appid:config.appId,
-			secret:config.appSecret,
-			js_code:jcode,
-			grant_type:'authorization_code'
-		
-		}
-	
-	}).then(function(response){
-  //console.log(response.data);//处理成功的函数 相当于success
-  v_data=response.data.openid
-})
-
-res.send(v_data);
-   
-
-}).listen(port);
+app.get('/', (req, res) => {
+    var params = req.body
+    var {code, type} = params
+    if (type === 'wxapp') {
+      axios.get('https://api.weixin.qq.com/sns/jscode2session', {
+        params: {
+          appid: config.appId,
+          secret: config.appSecret,
+          js_code: code,
+          grant_type: 'authorization_code'
+        }
+      }).then(({data}) => {
+        var openId = data.openid
+      }).then(() => {
+        res.send({
+          code: 0
+        })
+      })
+    } else {
+      throw new Error('未知的授权类型')
+    }
+  })
+.listen(port);
 
 // 监听端口，等待连接
 
